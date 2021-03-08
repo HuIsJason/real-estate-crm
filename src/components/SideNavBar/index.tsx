da1a9b18028458d1397e1b1822b3c70f2a342b17
ObaidaSaleh <46760451+ObaidaSaleh@users.noreply.github.com>
2021-03-07 19:24:59 -0500

    made nav bar collapsable

--- src/components/SideNavBar/index.tsx
+++ src/components/SideNavBar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 import {
     Drawer,
     CssBaseline,
@@ -9,46 +10,73 @@ import {
     Divider,
     ListItem,
     ListItemIcon,
-    ListItemText
+    ListItemText,
+    IconButton
 } from '@material-ui/core';
+
 import {
     SupervisorAccount as SupervisorAccountIcon,
-    AccountCircle as AccountCircleIcon
+    AccountCircle as AccountCircleIcon,
+    Menu as MenuIcon,
+    ChevronLeft as ChevronLeftIcon,
+    ChevronRight as ChevronRightIcon
 } from '@material-ui/icons';
 
+import { useTheme } from '@material-ui/core/styles';
 import useStyles from './styles';
 
 const PermNavBar: React.FC = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
+      <AppBar position="fixed" 
+      className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}>
         <Toolbar>
+
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+
+            <MenuIcon />
+          </IconButton>
           <Typography color="primary"variant="h6" noWrap>
             CLIENTS
           </Typography>
         </Toolbar>
-        
-        {/* <Button variant="outlined" color="primary" classes={{ root: classes.listButtonsHead}} className={classes.listButtons + " " + classes.editButton}>
-            Edit Clients
-        </Button>
-        
-        <Button variant="contained" color="primary" classes={{ root: classes.listButtonsHead}} className={classes.listButtons + " " + classes.addButton}>
-            + Add Client
-        </Button> */}
 
       </AppBar>
       <Drawer
         className={classes.drawer}
-        variant="permanent"
+        open={open}
+        variant="persistent"
         classes={{
           paper: classes.drawerPaper,
         }}
         anchor="left"
       >
-        <div className={classes.toolbar} />
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
         <Divider />
         <List>
 
