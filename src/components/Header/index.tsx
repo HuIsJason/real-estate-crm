c279e59e57143840795d460d1f6b17e824842ea9
Jenny <jennny.zhang@mail.utoronto.ca>
2021-03-10 20:48:27 -0500

    clean up

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,11 +6,6 @@ import useStyles from './styles';
 import { useUserContext } from '../../contexts/UserContext';
 import { Redirect } from 'react-router';
 
-/*
- * SINCE WE DONT HAVE A REAL HEADER COMPONENT FOR NOW, THIS WILL BE A SAMPLE COMPONENT FOR TESTING.
- * YOU CAN SEE HOW THE UserContext IS CONSUMED, AS WELL AS HOW TO USE THE GLOBAL THEME BY
- * PASSING THE PROP color="primary" IF YOU WANT AN ELEMENT TO HAVE THE NAVY BLUE.
- */
 const Header: React.FC<Props> = ({ title }: Props) => {
   const { user, logoutUser } = useUserContext();
 
@@ -21,25 +16,12 @@ const Header: React.FC<Props> = ({ title }: Props) => {
       <h1 className={classes.root}>{title}</h1>
       {user ? (
         user.type !== 'ADMIN' ? (
-          // <Typography variant="h4" color="primary">
-          //   The current user is: {(user as any).id}, {(user as any).firstName},{' '}
-          //   {(user as any).bio}, {(user as any).phone}
-          // </Typography>
           <Redirect to='/client-list'></Redirect>
         ) : (
-          // <Typography variant="h4" color="primary">
-          //   ADMIN LOGGED IN monkaS
-          // </Typography>
           <Redirect to='/admin'></Redirect>
         )
       ) : (
         <Typography variant="h4">not currently logged in</Typography>
-      )}
-      {user && (
-        <Button variant="contained" color="primary" onClick={logoutUser}>
-          {' '}
-          {user ? 'Log out' : 'Log in'}
-        </Button>
       )}
     </>
   );
