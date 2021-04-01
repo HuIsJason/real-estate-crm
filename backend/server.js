f60eec65079564f6d8afaa9b79b76b60826908b3
Jason Hu <jason.h.7249@gmail.com>
2021-04-01 02:34:50 -0400

    finished login/logout/signup routes

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,18 @@ const express = require('express');
 const app = express();
 
 // These will handle all our routes
-const admin = require("./api/routes/admin");
-const agent = require("./api/routes/agent");
-const authentication = require("./api/routes/authentication");
-const clients = require("./api/routes/clients");
-const projects = require("./api/routes/projects");
-const property = require("./api/routes/property");
-
+const admin = require('./api/routes/admin');
+const agent = require('./api/routes/agent');
+const authentication = require('./api/routes/authentication');
+const clients = require('./api/routes/clients');
+const projects = require('./api/routes/projects');
+const property = require('./api/routes/property');
 
 let cors = require('cors');
 app.use(cors());
 
 // mongoose and mongo connection
-const { mongoose } = require("./api/db/mongoose");
+const { mongoose } = require('./api/db/mongoose');
 mongoose.set('useFindAndModify', false); // for some deprecation issues
 
 // body-parser: middleware for parsing parts of the request into a usable object (onto req.body)
@@ -23,22 +22,24 @@ app.use(bodyParser.json()); // parsing JSON body
 app.use(bodyParser.urlencoded({ extended: true })); // parsing URL-encoded form data (from form POST requests)
 
 // express-session for managing user sessions
-const session = require("express-session");
-const MongoStore = require('connect-mongo') // to store session information on the database in production
+const session = require('express-session');
+const MongoStore = require('connect-mongo'); // to store session information on the database in production
 
 // Initialize a session and cookie
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET || "our hardcoded secret", // make a SESSION_SECRET environment variable when deploying (for example, on heroku)
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            expires: 60000,
-            httpOnly: true
-        },
-        // store the sessions on the database in production
-        store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/AgentAPI'})
-    })
+  session({
+    secret: process.env.SESSION_SECRET || 'our hardcoded secret', // make a SESSION_SECRET environment variable when deploying (for example, on heroku)
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60000,
+      httpOnly: true,
+    },
+    // store the sessions on the database in production
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/AgentAPI',
+    }),
+  })
 );
 
 // Middleware for handling all the various routes
@@ -50,9 +51,9 @@ app.use('/api/projects', projects);
 app.use('/api/property', property);
 
 // All routes other than above will go to index.html
-app.get("*", (req, res) => {
-    res.status(404).send("404 Not Found");
-    // TODO: We can add a cool 404 page here later
+app.get('*', (req, res) => {
+  res.status(404).send('404 Not Found');
+  // TODO: We can add a cool 404 page here later
 });
 
 module.exports = app;
